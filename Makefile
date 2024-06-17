363ac7bc1fb709af377052fe9f8558e2a16ac929
Conner Rose <conner.n.rose@gmail.com>
2024-06-17 18:31:05 -0400

    Add main.cpp and Makefile
    See main.cpp for a simple, multithreaded websocket echo server.

---
+++ Makefile
@@ -0,0 +1,25 @@
+CXX = g++
+CXXFLAGS = -march=native -O3 -Wpedantic -Wall -Wextra -Wsign-conversion \
+					 -Wconversion -std=c++20 \
+					 -flto -fsanitize=address -g
+					 # -lssl -lcrypto
+IFLAGS = -I/usr/local/include/uWebSockets -I/usr/local/include/uSockets \
+				 -isystem /usr/local/include/glaze
+LDFLAGS = uWebsockets/uSockets/*.o -lz
+
+SRC_DIR = src
+SRC_FILES = $(wildcard $(SRC_DIR)/*.cpp)
+EXECUTABLES = $(patsubst $(SRC_DIR)/%.cpp,%,$(SRC_FILES))
+
+
+all: $(EXECUTABLES)
+
+uSockets:
+	$(MAKE) -C uWebSockets/uSockets
+
+%: $(SRC_DIR)/%.cpp
+	$(CXX) $(CXXFLAGS) $(IFLAGS) $< $(LDFLAGS) -o $@
+
+clean:
+	rm -rf ./uWebSockets/uSockets/*.o ./uWebSockets/uSockets/uSockets.a
+	rm -rf $(EXECUTABLES) $(wildcard *.dSYM)
---
+++ src/main.cpp
@@ -0,0 +1,51 @@
+#include <algorithm>
+#include <thread>
+#include "App.h"
+
+constexpr int num_assets = 4;
+
+auto main() -> int {
+  struct PerSocketData {};
+
+  std::vector<std::thread*> threads(num_assets);
+
+  for (size_t i = 0; i < num_assets; ++i) {
+    threads[i] = new std::thread([i]() {
+      uWS::App()
+          .ws<PerSocketData>(
+              "/*", {
+                        .open =
+                            [](auto* ws) {
+                              std::cout << "Connected to thread "
+                                        << std::this_thread::get_id()
+                                        << std::endl;
+                              ws->send("Connected");
+                            },
+                        .message =
+                            [](auto* ws, std::string_view message,
+                               uWS::OpCode opCode) {
+                              std::cout << std::this_thread::get_id()
+                                        << " received message: " << message
+                                        << std::endl;
+                              ws->send(message, opCode);
+                            },
+                    })
+          .listen(9001 + static_cast<int>(i),
+                  [i](auto* listen_socket) {
+                    if (listen_socket) {
+                      std::cout << "Thread " << std::this_thread::get_id()
+                                << " listening on port "
+                                << 9001 + static_cast<int>(i) << std::endl;
+                    } else {
+                      std::cout << "Thread " << std::this_thread::get_id()
+                                << " failed to listen on port 9001"
+                                << std::endl;
+                    }
+                  })
+          .run();
+    });
+  }
+
+  std::for_each(threads.begin(), threads.end(),
+                [](std::thread* t) { t->join(); });
+}
